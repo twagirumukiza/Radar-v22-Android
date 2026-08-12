51ed7f0a567cfbd1654b6ff2b747b65fd6cea1c2
twagirumukiza <38731174+twagirumukiza@users.noreply.github.com>
2026-08-12 23:54:02 +0200

    Add files via upload

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.twagirumukiza.radar"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.twagirumukiza.radar"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 24
+        versionName = "23.2.0"
+    }
+}
